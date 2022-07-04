33973fe05c03aaa207c36c0ec6af1f440c488168
Sergei Pankov <sergeipankov@Sergeis-MacBook-Air.local>
2022-07-04 18:20:50 +0100

    config

--- CNS/Classes/Misc/GlobalDefinitions.swift
+++ CNS/Classes/Misc/GlobalDefinitions.swift
@@ -17,7 +17,7 @@ final class GlobalDefinitions {
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
-    static let appStoreUrl = "https://apps.apple.com/app/emt-prep/id1568136144" // TODO
+    static let appStoreUrl = "https://apps.apple.com/us/app/cns-exam/id1626950970"
     static let appleAppID = "1626950970"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
