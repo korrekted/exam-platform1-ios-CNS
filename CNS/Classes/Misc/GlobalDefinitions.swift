fe69dec01497bf14fa85773d3b6602cd0f5136e5
akonst17 <akonst17@gmail.com>
2022-09-22 11:57:15 +0300

    интеграция sdk 2.0; новые урлы; build 5

--- CNS/Classes/Misc/GlobalDefinitions.swift
+++ CNS/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams2.mastrapi.com"
     static let apiKey = "1hqVaCos43ek6QBY"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "gvKhgTOMU8ouk2lu"
-
     static let applicationTag = "EXAM_CNS"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "gvKhgTOMU8ouk2lu"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "8ALcRoTD5EmoSJCy"
 
     static let appStoreUrl = "https://apps.apple.com/us/app/cns-exam/id1626950970"
     static let appleAppID = "1626950970"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
